83c5e7cf62d2ef5443b7b3c8d1e45706c5c00bb7
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-02-26 23:47:11 +0900

    Example JS

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Momontum Clone 2021 Update
 
 Javascript is so much fun!
+
+# 메모장
+
+1. 공부 시작하는 부분: 2.5
---
+++ index.js
@@ -0,0 +1,7 @@
+const title = document.querySelector("#title");
+
+function handleClick() {
+    title.style.color = "green";
+}
+
+title.addEventListener("click", handleClick);
