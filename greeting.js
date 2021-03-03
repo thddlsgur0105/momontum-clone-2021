040fded3cc2d4ca57b7d6a4ce051729d87052270
thddlsgur0105 <70431221+thddlsgur0105@users.noreply.github.com>
2021-03-03 23:41:11 +0900

    Updated greeting.js

--- greeting.js
+++ greeting.js
@@ -5,6 +5,22 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
       SHOWING_CN = "showing"
 
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greetings.classList.add(SHOWING_CN);
@@ -15,7 +31,7 @@ function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
 
     if (currentUser == null) {
-
+        askForName()
     } else {
         paintGreeting(currentUser);
     }
